11f6c1929887502a5d6ad444d11ee0864bc47790
yuetan1988 <yuetann@outlook.com>
2024-04-08 10:55:59 +0800

    feat: update rag

--- app/app.py
+++ app/app.py
@@ -1,1 +1,106 @@
 import os
+import gradio as gr
+from rag.chain import ModelCenter
+
+
+class CFG:
+    llm_model_name_or_path = "../models"
+    embed_model_name_or_path = "BAAI/bge-large-zh-v1.5"
+    vector_db_path = "../examples/database/chroma"
+
+
+def upload_file(file):
+    """用户上传"""
+    if not os.path.exists("docs"):
+        os.mkdir("docs")
+    filename = os.path.basename(file.name)
+    shutil.move(file.name, "docs/" + filename)
+    # file_list首位插入新上传的文件
+    file_list.insert(0, filename)
+    application.source_service.add_document("docs/" + filename)
+    return gr.Dropdown.update(choices=file_list, value=filename)
+
+
+def clear_session():
+    return "", None, ""
+
+
+def main():
+    model_center = ModelCenter(CFG)
+
+    block = gr.Blocks()
+    with block as demo:
+        with gr.Row(equal_height=True):
+            with gr.Column(scale=15):
+                gr.Markdown(
+                    """<h1><center>LayerLLM powered by InternLM</center></h1>"""
+                    # <center>书生浦语</center>
+                    # """
+                )
+            # gr.Image(value=LOGO_PATH, scale=1, min_width=10,show_label=False, show_download_button=False)
+
+        with gr.Row():
+            with gr.Column(scale=1):
+                embedding_model = gr.Dropdown(
+                    ["text2vec-base", "bge-base"],
+                    label="向量模型",
+                    value="text2vec-base",
+                )
+                chat = gr.Dropdown(["InternLM"], label="大语言模型", value="InternLM")
+                top_k = gr.Slider(
+                    1, 20, value=3, step=1, label="检索topk文档", interactive=True
+                )
+                set_kg_btn = gr.Button("加载知识库")
+                file = gr.File(
+                    label="上传文件到知识库",
+                    visible=True,
+                    file_types=[".txt", ".md", ".docx", ".pdf"],
+                )
+
+            with gr.Column(scale=4):
+                with gr.Row():
+                    chatbot = gr.Chatbot(height=400, show_copy_button=True)
+                with gr.Row():
+                    # 创建一个文本框组件，用于输入 prompt。
+                    message = gr.Textbox(label="请输入问题")
+                with gr.Row():
+                    # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                    clear = gr.ClearButton(components=[chatbot], value="清除历史")
+                    # 创建提交按钮。
+                    db_wo_his_btn = gr.Button("生成对话")
+                with gr.Row():
+                    # 召回参考文献
+                    search = gr.Textbox(label="引用文献", max_lines=10)
+
+            # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+            file.upload(upload_file, inputs=file, outputs=None)
+
+            # 点击chat按钮
+            db_wo_his_btn.click(
+                model_center.qa_chain_self_answer,
+                inputs=[message, chatbot],
+                outputs=[message, chatbot, search],
+            )
+            # 输入框回车
+            message.submit(
+                model_center.qa_chain_self_answer,
+                inputs=[message, chatbot],
+                outputs=[message, chatbot, search],
+            )
+
+        gr.Markdown(
+            """提醒：<br>
+            1. 本页面是名为罪恶克星的demo, 结果输出仅供参考, 源码[](), 感谢书生浦语大模型提供的算力与模型支持。 <br>
+            """
+        )
+    # threads to consume the request
+    gr.close_all()
+
+    # 启动新的 Gradio 应用，设置分享功能为 True，并使用环境变量 PORT1 指定服务器端口。
+    # demo.launch(share=True, server_port=int(os.environ['PORT1']))
+    demo.queue()
+    demo.launch()
+
+
+if __name__ == "__main__":
+    main()
---
+++ app/rag/chain.py
@@ -0,0 +1,92 @@
+from langchain.chains import RetrievalQA
+from langchain.retrievers import EnsembleRetriever
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.llms.base import LLM
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    UnstructuredFileLoader,
+    UnstructuredMarkdownLoader,
+)
+from langchain_community.vectorstores import Chroma
+from rag.llm import InternLLM
+
+
+def get_prompt():
+    template = """请使用以下提供的上下文来回答用户的问题。如果无法从上下文中得到答案，请回答你不知道，并总是使用中文回答。
+    提供的上下文：
+    ···
+    {context}
+    ···
+    用户的问题: {question}
+    你给的回答:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate(
+        input_variables=["context", "question"], template=template
+    )
+    return QA_CHAIN_PROMPT
+
+
+def get_retrieval():
+    return
+
+
+def load_chain(CFG):
+    embeddings = HuggingFaceEmbeddings(model_name=CFG.embed_model_name_or_path)
+
+    persist_directory = CFG.vector_db_path
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embeddings,
+    )
+
+    llm = InternLLM(model_name_or_path=CFG.llm_model_name_or_path)
+    QA_CHAIN_PROMPT = get_prompt()
+
+    retriever = vectordb.as_retriever(search_type="mmr")
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+    )
+
+    qa_chain.return_source_documents = True
+    return qa_chain
+
+
+def parse_reference(response):
+    reference_text = ""
+    for idx, source in enumerate(response["source_documents"][:4]):
+        sep = f"参考文献【{idx + 1}】：{source.metadata['header1']}"
+        reference_text += f"{sep}\n{source.page_content}\n\n"
+    return reference_text
+
+
+class ModelCenter:
+    """
+    存储问答 Chain 的对象
+    """
+
+    def __init__(self, CFG):
+        self.chain = load_chain(CFG)
+
+    def qa_chain_self_answer(self, query: str, chat_history: list = []):
+        """
+        调用不带历史记录的问答链进行回答
+        """
+        reference = []
+        if query == None or len(query) < 1:
+            return "", chat_history, reference
+        try:
+            result = self.chain({"query": query})
+            chat_history.append((query, result["result"]))
+            reference = parse_reference(result)
+            return "", chat_history, reference
+        except Exception as e:
+            return e, chat_history, reference
